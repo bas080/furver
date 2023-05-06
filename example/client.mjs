a105dbdf2318a32e65976c248348747e059a580f
bas081 <bas080@hotmail.com>
2023-05-06 18:40:07 +0200

    Fix the client example and code hl

--- example/client.mjs
+++
@@ -1,9 +0,0 @@
-import FurverClient from '../client.mjs'
-
-const api = await FurverClient({endpoint: `http://localhost:${process.env.PORT}`})
-
-console.log(await Promise.all([
-  api.identity('hello world'),
-  api.timestamp(),
-  api.version()
-]))
