170f56c8df6b7ac7b0be6bb5961916b64df58704
bas081 <bas080@hotmail.com>
2023-05-07 18:34:39 +0200

    Remove unused script file

--- schema-cli.mjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import schema from './schema.mjs';
-
-(async function () {
-  const { default: api } = await import(process.argv[2])
-  console.log(JSON.stringify(schema(api)))
-})()
